6faa10186a8311b7f99a3f860bf1d552f54ebc69
BruceHill <brucehillwalley@gmail.com>
2023-12-06 10:37:13 +0300

    logic error fixed

--- src/App.js
+++ src/App.js
@@ -1,36 +1,27 @@
-import React, { useState } from 'react';
-import List from './components/List';
-import data from "./helper/data"
+import React, { useState } from "react";
+import List from "./components/List";
+import data from "./helper/data";
 
 function App() {
-    const [index, setIndex]=useState(0)
-const indexPrev=()=>{
- 
-     setIndex(data.length-Math.abs(index-5))
- 
- 
-}
-const indexNext=()=>{
-  
-    setIndex((index+5)%data.length)
-  }
- 
-
-
-
+  const [index, setIndex] = useState(0);
+  const indexPrev = () => {
+    setIndex((index - 5 + data.length) % data.length);
+  };
+  const indexNext = () => {
+    setIndex((index + 5) % data.length);
+  };
 
   return (
- 
     <main>
       <section className="container">
         <h3>Employee List</h3>
         <h5>
-          (Employees {index+1} to {index+5})
+          (Employees {index + 1} to {index + 5})
         </h5>
-        <List data={data.slice(index, index+5)} />
-        <div className='btns'>
-          <button onClick={indexPrev} >Prev</button>
-          <button onClick={indexNext} >Next</button>
+        <List data={data.slice(index, index + 5)} />
+        <div className="btns">
+          <button onClick={indexPrev}>Prev</button>
+          <button onClick={indexNext}>Next</button>
         </div>
       </section>
     </main>
